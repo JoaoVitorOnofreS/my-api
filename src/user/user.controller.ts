6e2f5b337f21aa830a5b35d37c1edbd59bf9fe76
JoaoVitorOnofreS <joao.santosp9@hotmail.com>
2024-10-04 19:56:10 -0300

    feat: Create DB connection, and put some data.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
-
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Post,
+  Put,
+  Param,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { Prisma } from '@prisma/client';
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @Inject()
+  private readonly UserService: UserService;
+
+  @Get()
+  async findAll() {
+    return this.UserService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.UserService.findOne(id);
+  }
+
+  @Post('create')
+  async create(@Body() data: Prisma.UserCreateInput) {
+    return this.UserService.create(data);
+  }
+
+  @Put('update/:id')
+  async update(@Param('id') id: string, @Body() data: Prisma.UserUpdateInput) {
+    return this.UserService.update(id, data);
+  }
+
+  @Delete('delete/:id')
+  @HttpCode(HttpStatus.OK)
+  async delete(@Param('id') id: string) {
+    await this.UserService.delete(id);
+
+    return {
+      statusCode: 200,
+      message: 'Usuario deletado',
+    };
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { PrismaService } from '../database/prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  @Inject()
+  private readonly prisma: PrismaService;
+
+  async findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  async findOne(id: string) {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    return this.prisma.user.create({ data });
+  }
+
+  async update(id: string, data: Prisma.UserUpdateInput) {
+    return this.prisma.user.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async delete(id: string) {
+    return this.prisma.user.delete({ where: { id } });
+  }
+}
